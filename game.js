861ab669efdbc1a443c4957328840cd1ce85d9cc
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-11-02 20:04:52 +0100

    Gave the bouncing off feature to bounce exactly from the wall

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
-var x = canvas.width / 2;
-var dx = 2;
-var y = canvas.height - 3;
-var dy = -2;
-
 var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
 function drawBall() {
   // Drawing the ball
@@ -17,20 +20,45 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   // Clearing the ball's tail
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-  x += dx;
-  y += dy;
 
   // Change the ball's directions
-  if (x + dx > canvas.width || x + dx < 0) {
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-
-  if (y + dy > canvas.height || y + dy < 0) {
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
+  }
+
+  x += dx;
+  y += dy;
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
   }
 }
 setInterval(draw, 10);
