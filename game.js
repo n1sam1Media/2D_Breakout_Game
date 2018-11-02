f3a54385b25f237f96fd80bf03401e37dd497215
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-11-02 05:18:41 +0100

    Added bouncing off feature to the ball

--- game.js
+++ game.js
@@ -6,10 +6,12 @@ var dx = 2;
 var y = canvas.height - 3;
 var dy = -2;
 
+var ballRadius = 10;
+
 function drawBall() {
   // Drawing the ball
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -21,5 +23,14 @@ function draw() {
   drawBall();
   x += dx;
   y += dy;
+
+  // Change the ball's directions
+  if (x + dx > canvas.width || x + dx < 0) {
+    dx = -dx;
+  }
+
+  if (y + dy > canvas.height || y + dy < 0) {
+    dy = -dy;
+  }
 }
 setInterval(draw, 10);
