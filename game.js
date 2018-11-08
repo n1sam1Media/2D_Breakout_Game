501d37bf61d8a9afd0ad745506356ad3a06529ed
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-11-08 21:35:14 +0100

    Fixed minor issue of mouse control

--- game.js
+++ game.js
@@ -47,9 +47,9 @@ function keyUpHandler(e) {
   }
 }
 
-functionmouseMoveHandler(e) {
+function mouseMoveHandler(e) {
   var relativeX = e.clientX - canvas.offsetLeft;
-  if(relativeX > 0 && relativeX < canvas.width) {
+  if (relativeX > 0 && relativeX < canvas.width) {
     paddleX = relativeX - paddleWidth / 2;
   }
 }
